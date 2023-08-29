c95022496c37c34e085b9f9e3dd6f6ce940f5755
Gildesio-af <gildesioaf@alu.ufc.br>
2023-08-29 16:28:59 -0300

    Create Lista01 - Tecnicas de Programação

---
+++ Lista01/src/questoes/ExecutarQuestoes.java
@@ -0,0 +1,11 @@
+package questoes;
+
+public class ExecutarQuestoes {
+
+	public static void main(String[] args) {
+		Questoes q = new Questoes();
+		
+		q.questao1();
+	}
+
+}
---
+++ Lista01/src/questoes/Questoes.java
@@ -0,0 +1,296 @@
+package questoes;
+
+import java.util.Scanner;
+
+public class Questoes {
+	Scanner teclado = new Scanner(System.in);
+	public void questao1() {
+		System.out.print("Digite um numero: ");
+		int A = teclado.nextInt();
+		System.out.print("Digite outro numero: ");
+		int B = teclado.nextInt();
+		
+		if(A > 10) {
+			System.out.println("A > 10");
+			if(A + B == 20)
+				System.out.println("A + B == 20");
+			else
+				System.out.println("Numero nao valido");
+		}
+	}
+	
+	public void questao2() {
+		System.out.print("Digite um numero: ");
+		int A = teclado.nextInt();
+		System.out.print("Digite outro numero: ");
+		int B = teclado.nextInt();
+		
+		if(A < 10) {
+			System.out.println("A < 10");
+		}else if(A + B == 20) {
+			System.out.println("A + B == 20");
+		}else 
+			System.out.println("Numero nao valido");
+	}
+	
+	
+	public void questao3() {
+		System.out.print("Digite um numero: ");
+		int A = teclado.nextInt();
+		System.out.print("Digite outro numero: ");
+		int B = teclado.nextInt();
+		
+		if(A == 10) {
+			System.out.println("A == 10");
+			if(A + B == 20)
+				System.out.println("A + B == 20");
+			if(B == 10)
+				System.out.println("B == 10");
+		}
+	}
+	
+	public void questao4() {
+		System.out.print("Digite um numero: ");
+		int A = teclado.nextInt();
+		System.out.print("Digite outro numero: ");
+		int B = teclado.nextInt();
+		
+		if(A > 10 || A + B == 20) {
+			System.out.println("Numero valido");
+		} else if(A == B) {
+			System.out.println("A eh igual B");
+			System.out.println("A e B sao diferentes de 10");
+			System.out.println("A eh menor que 10");
+		}else 
+			System.out.println("Numero nao valido");
+	}
+	
+	public void questao5() {
+		System.out.print("Digite um numero: ");
+		int A = teclado.nextInt();
+		System.out.print("Digite outro numero: ");
+		int B = teclado.nextInt();
+		
+		if(A > 10 || A + B == 20)
+			System.out.println("Numero valido");
+		else
+			System.out.println("Numero nao valido");
+	}
+	
+	public void questao6() {
+		System.out.print("Digite um numero: ");
+		int A = teclado.nextInt();
+		System.out.print("Digite outro numero: ");
+		int B = teclado.nextInt();
+		
+		if(A > 10)
+			System.out.println("A > 10");
+		else
+			System.out.println("A <= 10");
+		if(A + B == 20) {
+			System.out.println("A + B == 20");
+		}else
+			System.out.println("A + B != 20");
+	}
+	
+	public void questao7() {
+		System.out.print("Digite um numero: ");
+		int A = teclado.nextInt();
+		System.out.print("Digite outro numero: ");
+		int B = teclado.nextInt();
+		
+		if(A > 10 || A + B == 20) {
+			System.out.println("Numeros validos");
+			if(A <= 10)
+				System.out.println("Numero nao valido");
+		}else
+			System.out.println("Numero nao valido");
+	}
+	
+	public void questao8() {
+		System.out.print("Digite um numero: ");
+		int A = teclado.nextInt();
+		System.out.print("Digite outro numero: ");
+		int B = teclado.nextInt();
+		
+		if(A > 10) {
+			System.out.println("A > B");
+			if(A + B == 20) {
+				System.out.println("A + B == 20");
+			}
+		}else
+			System.out.println("Numero nao valido");
+	}
+	
+	public void questao9() {
+		System.out.print("Digite um numero: ");
+		int A = teclado.nextInt();
+		System.out.print("Digite outro numero: ");
+		int B = teclado.nextInt();
+		
+		if(A > 10 && A + B == 20)
+			System.out.println("A + B == 20");
+		else
+			System.out.println("Numero nao valido");
+	}
+	
+	public void questao10() {
+		System.out.print("Digite um numero: ");
+		int A = teclado.nextInt();
+		System.out.print("Digite outro numero: ");
+		int B = teclado.nextInt();
+		
+		if(A <= 10) {
+			System.out.println("Numero menor que 10");
+		}
+		if(A + B != 20) {
+			System.out.println("Numero diferente de 20");
+		}
+	}
+	
+	public void questao11(){
+		System.out.print("Digite um numero: ");
+		int A = teclado.nextInt();
+		System.out.print("Digite outro numero: ");
+		int B = teclado.nextInt();
+		
+	    if(A <= 10){
+	        if(A + B == 20){
+	            System.out.println("A + B == 20");
+	        }else
+	            System.out.println("Numero nao valido");
+	    }
+	}
+
+	public void questao12(){
+		System.out.print("Digite um numero: ");
+		int A = teclado.nextInt();
+		System.out.print("Digite outro numero: ");
+		int B = teclado.nextInt();
+		
+	    if(A > 10)
+	        System.out.println("A > 10");
+	    if(A + B == 20)
+	        System.out.println("A + B == 20");
+	    else if(A <= 10)
+	        System.out.println("Numeros nao validos");
+
+	    System.out.println("Sejam bem-vindos a disciplina de tecnicas de programacao");
+	}
+
+	public void questao13(){
+		System.out.print("Digite um numero: ");
+		int A = teclado.nextInt();
+		System.out.print("Digite outro numero: ");
+		int B = teclado.nextInt();
+		
+	    if(A > 10){
+	         System.out.println(A > 10);
+	    if(A + B == 20)
+	    	System.out.println("A + B == 20");
+	    }else if(A <= 10)
+	    	System.out.println("Numeros nao validos");
+	}
+	
+	public void questao14() {
+		System.out.print("Digite um numero: ");
+		int A = teclado.nextInt();
+		System.out.print("Digite outro numero: ");
+		int B = teclado.nextInt();
+		
+		if(A > 10) 
+			System.out.println("A > 10");
+		if(A + B == 20)
+			System.out.println("A + B == 20");
+		else if(A <= 10)
+			System.out.println("Numero nao valido");
+	}
+	
+	public void questao15() {
+		System.out.print("Digite um numero: ");
+		int A = teclado.nextInt();
+		System.out.print("Digite outro numero: ");
+		int B = teclado.nextInt();
+		
+		if(A < 10) 
+			System.out.println("A < 10");
+		if(A + B == 20)
+			System.out.println("A + B == 20");
+		else if(A <= 10)
+			System.out.println("Numero nao valido");
+	}
+	
+	public void questao16() {
+		System.out.print("Digite um numero: ");
+		int A = teclado.nextInt();
+		System.out.print("Digite outro numero: ");
+		int B = teclado.nextInt();
+		
+		if(A == 10)
+			System.out.println("A == 10");
+		if(A + B == 20)
+			System.out.println("A + B = 20");
+		if(B == 10)
+			System.out.println("B == 10");
+	}
+	
+	public void questao17() {
+		System.out.print("Digite um numero: ");
+		int A = teclado.nextInt();
+		System.out.print("Digite outro numero: ");
+		int B = teclado.nextInt();
+		
+		if(A > 10 || A + B == 20)
+			System.out.println("Numero valido");
+		else if(A == B) {
+			System.out.println("A eh igual B");
+		}else if(A != 10 && B != 10 && A < 10)
+			System.out.println("A < 10");
+		else
+			System.out.println("Numero nao valido");
+	}
+	
+	public void questao18() {
+		System.out.print("Digite um numero: ");
+		int A = teclado.nextInt();
+		System.out.print("Digite outro numero: ");
+		int B = teclado.nextInt();
+		
+		if(A > 10 || A + B == 20) {
+			System.out.println("Numero valido");
+		}else 
+			System.out.println("Numero nao valido");
+	}
+	
+	public void questao19() {
+		System.out.print("Digite um numero: ");
+		int A = teclado.nextInt();
+		System.out.print("Digite outro numero: ");
+		int B = teclado.nextInt();
+		
+		if(A > 10) {
+			System.out.println("A > 10");
+		}else
+			System.out.println("A <= 10");
+		if(A + B == 20)
+			System.out.println("A + B == 20");
+		else
+			System.out.println("A + B != 20");
+	}
+	
+	public void questao20() {
+		System.out.print("Digite um numero: ");
+		int A = teclado.nextInt();
+		System.out.print("Digite outro numero: ");
+		int B = teclado.nextInt();
+		
+		if(A > 10 || A + B == 20) {
+			System.out.println("Numeros validos");
+		}else
+			System.out.println("Numero nao valido");
+		
+		System.out.println("Sejam bem-vindos a disciplina"
+				+ " de tecnicas de programacao");
+	}
+}
+
